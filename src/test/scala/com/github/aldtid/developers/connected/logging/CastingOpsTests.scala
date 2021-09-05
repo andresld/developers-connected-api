1757db422c82602e7107a920c36429fdc524435f
Andrés López Díaz <andres.lopezdiaz@telefonica.com>
2021-09-05 11:35:50 +0200

    Fix test error.

--- src/test/scala/com/github/aldtid/developers/connected/logging/CastingOpsTests.scala
+++ src/test/scala/com/github/aldtid/developers/connected/logging/CastingOpsTests.scala
@@ -6,11 +6,11 @@ import org.scalatest.matchers.should.Matchers
 
 class CastingOpsTests extends AnyFlatSpec with Matchers {
 
-  object ops$$ extends CastingOps
+  object ops extends CastingOps
 
   "loggableAsLog" should "transform an instance into a Log structure as expected" in {
 
-    import ops$$._
+    import ops._
 
     implicit val log: Log[String] = Log.createLog[String]("", "log: " + _)(_ + "" + _)
     implicit val loggable: Loggable[Int, String] = _.toString
